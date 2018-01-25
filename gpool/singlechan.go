79ec7c3a3fc4eba80a978431c41147ed279769af
seven <842375454@qq.com>
2018-01-25 21:21:42 +0800

    update

--- gpool/singlechan.go
+++ gpool/singlechan.go
@@ -4,13 +4,23 @@ import (
 	"fmt"
 )
 
-// type Single struct {
-
-// }
-
 const (
 	Max = 10
 )
+
+type SinglePool struct {
+	queue chan int
+}
+
+func NewSinglePool(quNum int) *SinglePool {
+	return &SinglePool{
+		queue: make(chan int, quNum),
+	}
+}
+
+func StartWork() {
+
+}
 
 func main() {
 	fmt.Println("start...")
